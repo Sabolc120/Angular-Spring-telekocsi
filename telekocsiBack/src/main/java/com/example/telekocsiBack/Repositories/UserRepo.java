f6922f70304bd0a83a61bec45baf6f5dfb7914a8
Sabolc120 <87311746+Sabolc120@users.noreply.github.com>
2022-09-08 12:47:36 +0200

    Delete telekocsiBack directory

--- telekocsiBack/src/main/java/com/example/telekocsiBack/Repositories/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.telekocsiBack.Repositories;
-
-import com.example.telekocsiBack.Model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<UserModel, Long> {
-}
